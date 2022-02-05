6dfa7d73ff786ac677325e70ee139dead9e538b2
Jay Parmar <jayhparmar.0707@gmail.com>
2022-02-05 08:49:56 -0500

    Add createContact, searchContact, deleteUser func

--- js/handler.js
+++ js/handler.js
@@ -66,31 +66,131 @@ function handleRegister() {
         login: username,
         password: md5(password),
     };
-    doRequest(registerUser, "SignUp", params);
-}
 
-function registerUser(resp) {
-    if (!resp || resp.error) {
-        console.log(resp.error);
-        document.querySelector("#register-result").innerHTML =
-            "Error: " + resp.error;
-        window.location.href = "index.html";
-        return;
-    }
-
-    setCookie(resp.id);
-    window.location.href = "contacts.html";
+    fetch(urlBase + "SignUp" + ext, {
+        method: "POST",
+        body: JSON.stringify(params),
+    })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            if (resp.error) throw Error(resp.error);
+            setCookie(resp.id);
+            window.location.href = "contacts.html";
+        })
+        .catch((err) => {
+            document.querySelector("#register-result").innerHTML =
+                "Error: " + err;
+        });
 }
 
 // Adds a contact, then prepares it for editing
 function handleCreateContact() {
-    if (document.getElementById("info").classList.contains("info-selected")) {
-        return;
-    }
-
     // Open contact tab
     document.getElementById("info").classList.add("info-selected");
     const cid = createContact(getID, "", "", "", "");
+
+    let uid = getID();
+    let firstName = document.querySelector("#editContactFirstName").value;
+    let lastName = document.querySelector("#editContactLastName").value;
+    let email = document.querySelector("#editContactEmail").value;
+    let phone = document.querySelector("#editContactPhone").value;
+
+    if (
+        firstName.length == 0 ||
+        lastName.length == 0 ||
+        email.length == 0 ||
+        phone.length == 0
+    ) {
+        document.querySelector("#create-result").innerHTML =
+            "Please enter all information.";
+        return;
+    }
+
+    const params = {
+        uid: uid,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phone: phone,
+        color: "#" + ((Math.random() * 0xffffff) << 0).toString(16),
+    };
+
+    fetch(url + "CreateContact" + ext, {
+        method: "POST",
+        body: json.stringify(params),
+    })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            if (resp.error) throw Error(resp.error);
+            document.querySelector("#create-result").innerHTML =
+                "Contact created";
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+// Deletes a user
+function deleteUser() {
+    let uid = getID();
+    let currentUser = getUser(uid);
+
+    const params = {
+        password: currentUser.password,
+        uid: uid,
+    };
+
+    fetch(urlBase + "DeleteUser" + ext, {
+        method: "POST",
+        body: JSON.stringify(params),
+    })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            if (resp.error) throw Error(resp.error);
+            window.location.href = "index.html";
+            document.querySelector("#login-result").innerHTML = "User Deleted";
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function searchContacts() {
+    let uid = getID();
+    let query = document.querySelector("#searchContact").value;
+
+    if (query.length == 0) {
+        document.querySelector("#searchContact").placeholder =
+            "Please enter a valid string";
+        return;
+    }
+
+    const params = {
+        uid: uid,
+        query: query,
+    };
+
+    fetch(urlBase + "SearchContacts" + ext, {
+        method: "POST",
+        body: json.stringify(params),
+    })
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+            let arr = resp.results;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i].cid);
+        console.log(arr[i].firstName);
+        console.log(arr[i].lastName);
+    }
 }
 
 function logOut() {
